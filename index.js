d883e8c02cdfb89d9ccde6fe0185c2b30eddb82e
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-26 15:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var height = "74"
 
 if (name === 'Susan'){
   console.log('Name is correct');
+} else {
+  console.log('Expected name to be susan');
 }
 
 // Don't worry about this
 module.exports = { name, height
 }
-
